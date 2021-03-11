bae47f26b760ad8228ca642a30125df6871cdff3
William Entriken <github.com@phor.net>
2021-03-10 22:14:40 -0500

    Create project

---
+++ demo/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use \GenoBankio\Certificates;
+
+// Program inputs //////////////////////////////////////////////////////////////
+$network = Certificates\Model\Network::getTest();
+
+$twelveWordPassphrase = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
+
+$certificate = [
+
+];
+
+
+echo 'hi';
+
+var_dump($network);
